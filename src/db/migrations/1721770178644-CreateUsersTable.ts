ca4ec12e91111837b5070e0828460510dc14895a
Edwin Gallegos Hernández <edwin.mghdez@gmail.com>
2024-07-28 17:15:47 -0600

    fix: value null on create user

--- src/db/migrations/1721770178644-CreateUsersTable.ts
+++ src/db/migrations/1721770178644-CreateUsersTable.ts
@@ -11,7 +11,7 @@ export class CreateUsersTable1721770178644 implements MigrationInterface {
                 \`email\` varchar(255) NOT NULL,
                 \`password\` varchar(255) NOT NULL,
                 \`created_at\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                \`updated_at\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                \`updated_at\` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
                 PRIMARY KEY (\`id\`)
             )`
         );
